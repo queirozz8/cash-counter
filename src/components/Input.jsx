c41392c5c0b8f290a2fe2a70fd2e52db232eca14
Henrique Queiroz <zeccakut@gmail.com>
2025-01-10 03:01:59 -0300

    Update Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,22 +1,26 @@
-export default function Input({ billValue, bill, calculateValue }) {
+export default function Input({ billValue, billName, inputBill, calculateValue }) {
+
   function verifyInput(e) {
-    const allowedKeys = ['Backspace', 'Delete', 'Tab', 'Ctrl', 'ArrowLeft', 'ArrowRight']
+    const allowedKeys = ['Backspace', 'Delete', 'Tab', 'Ctrl', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown']
     const isCtrlPressed = e.ctrlKey
 
     if ((isCtrlPressed && e.key === 'c') || (isCtrlPressed && e.key === 'v')) return;
     if ((e.target.length >= 8) || (!allowedKeys.includes(e.key) && isNaN(e.key))) e.preventDefault()
   };
+  
+  function handleChange(e) { calculateValue(e.target.value, billName) };
 
   return (
     <div className="flex justify-center items-center gap-3">
-      <label className="flex justify-center items-center text-xl text-zinc-300" htmlFor={bill}>R$ {bill.replace('.', ',')}</label>
+      <label className="flex justify-center items-center text-xl text-zinc-300" htmlFor={inputBill}>R$ {inputBill.replace('.', ',')}</label>
         <input
           className="p-4 text-zinc-300 bg-[#1D2623] border border-[#354942] rounded-xl placeholder:text-zinc-600"
           type="text"
           onKeyDown={verifyInput}
-          onChange={calculateValue}
+          onChange={handleChange}
           value={billValue}
-          id={bill}
+          name={billName}
+          id={inputBill}
           maxLength={8}
           placeholder="Digite aqui as unidades"
         />
