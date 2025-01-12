5c7d75b583a76959e4d368e6ce53586503ad75b0
Henrique Queiroz <zeccakut@gmail.com>
2025-01-12 02:10:57 -0300

    Update Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,14 +2,14 @@ import { Trash2 } from 'lucide-react'
 export default function Input({ setBills, billValue, billName, inputBill, calculateValue }) {
 
   function verifyInput(e) {
-    const allowedKeys = ['Backspace', 'Delete', 'Tab', 'Ctrl', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown']
-    const isCtrlPressed = e.ctrlKey
-
-    if ((isCtrlPressed && e.key === 'c') || (isCtrlPressed && e.key === 'v')) return;
-    if ((e.target.length >= 8) || (!allowedKeys.includes(e.key) && isNaN(e.key))) e.preventDefault()
+    const notAllowedKeys = ['e', 'E', '+', '-']
+    if ((e.target.length >= 8) || (notAllowedKeys.includes(e.key))) e.preventDefault()
   };
   
-  function handleChange(e) { calculateValue(Number(e.target.value), billName) };
+  function handleChange(e) { 
+    if (e.target.value.startsWith('-')) e.preventDefault()
+    else calculateValue(Number(e.target.value), billName) 
+  };
 
   function handleClick() {
     setBills(prevValues => ({
@@ -22,8 +22,8 @@ export default function Input({ setBills, billValue, billName, inputBill, calcul
     <div className="flex justify-center items-center md:gap-3 w-72 md:w-96">
       <label className="flex justify-center items-center text-sm md:text-xl text-zinc-300" htmlFor={inputBill}>R$ {inputBill.replace('.', ',')}</label>
         <input
-          className="w-36 md:w-auto p-1 md:p-4 text-zinc-300 bg-[#1D2623] border border-[#354942] rounded-xl placeholder:text-zinc-600"
-          type="text"
+          className="w-36 md:w-auto p-1 md:p-4 text-zinc-300 bg-[#1D2623] border border-[#354942] rounded-xl placeholder:text-sm placeholder:text-zinc-600"
+          type="number"
           onKeyDown={verifyInput}
           onChange={handleChange}
           value={billValue}
