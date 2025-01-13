97980d459a91c07d1d4c854efe72f6b41097a63d
Henrique Queiroz <zeccakut@gmail.com>
2025-01-13 02:03:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ export default function App() {
 
   return (
     <div className='flex flex-col items-center md:mt-14 w-screen h-screen'>
-      <div className='flex flex-col justify-center items-center relative md:static left-16'>
+      <div className='flex flex-col justify-center items-center relative md:static left-[4.5rem]'>
         <h1 className='flex justify-center items-center md:gap-4 font-bold md:text-2xl lg:text-4xl text-zinc-300'>Contador de Dinheiro Vivo <Coins size={48} color="#82ff4d" strokeWidth={0.8} /></h1>
         <p className='w-[420px] text-sm md:text-base text-zinc-300'>Selecione a quantidade de cada cédula/moeda que possui.</p>
       </div>
@@ -92,7 +92,7 @@ export default function App() {
         <Input setBills={setBills} billValue={bills.moeda5} billName="moeda5" inputBill="0.05" setResult={setResult} calculateValue={calculateValue} />
       </form>
 
-      <div className='flex gap-7 md:gap-10 mt-36 md:mt-44 mb-6 md:mb-10'>
+      <div className='flex gap-7 md:gap-10 relative md:static left-5 mt-36 md:mt-44 mb-6 md:mb-10'>
         <button className='p-2 md:p-4 text-zinc-300 bg-red-800 border border-red-600 rounded-xl' onClick={handleClear}>Limpar</button>
         <p className='flex justify-center items-center relative md:left-8 text-zinc-300'>R$</p>
         <input className='md:w-96 md:h-14 p-2 md:p-3 text-zinc-300 border border-[#354942] bg-[#1D2623] rounded-xl placeholder:text-zinc-600' value={result} placeholder='Resultado' readOnly id="input" />
